82e4d62d60b3f9ab0e53c67d58caabaefea0537b
asimaranov <a.simaranov@gmail.com>
2024-11-29 14:33:02 +0700

    Fixed methods

--- api_server/methods.py
+++ api_server/methods.py
@@ -9,23 +9,21 @@ from mongo import db
 
 async def process_webhook(request: BaseRequest):
     request_json = await request.json()
-    await db.requests.insert_one(request_json)
-
     omi_id = request.rel_url.query['uid']
+
+    await db.requests.insert_one({'request': request_json, 'omi_id': omi_id})
 
     if request_json['structured']:
         memory = Memory.model_validate(request_json)
-        await db.memories.insert_one(memory)
+        await db.memories.insert_one({'memory': memory.model_dump(), 'omi_id': omi_id})
 
         link = await db.links.find_one({'omi_id': omi_id})
         telegram_id = link['telegram_id']
 
         await bot.send_message(telegram_id, f'{memory.structured.emoji} <b>{memory.structured.title}</b>\n{memory.structured.overview}')
-
-        
     else:
         realtime = RealtimePluginRequest.model_validate(request_json)
-        await db.realtime.insert_one(realtime)
+        await db.realtime.insert_one({'realtime': realtime.model_dump(), 'omi_id': omi_id})
 
     return web.json_response(
         {
