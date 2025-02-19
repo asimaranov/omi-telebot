10ce8ffe189066860d1c913f096d561079088d21
asimaranov <a.simaranov@gmail.com>
2025-02-19 10:18:28 +0300

    Fixed dropped request

--- api_server/methods.py
+++ api_server/methods.py
@@ -11,8 +11,6 @@ async def process_webhook(request: BaseRequest):
     request_json = await request.json()
     omi_id = request.rel_url.query['uid']
 
-    await db.requests.insert_one({'request': request_json, 'omi_id': omi_id})
-
     if 'structured' in request_json:
         saved_request = await db.requests.find_one({'request.id': request_json['id']})
         print(saved_request)
@@ -36,6 +34,8 @@ async def process_webhook(request: BaseRequest):
     else:
         realtime = RealtimePluginRequest.model_validate(request_json)
         await db.realtime.insert_one({'realtime': realtime.model_dump(), 'omi_id': omi_id})
+    
+    await db.requests.insert_one({'request': request_json, 'omi_id': omi_id})
 
     return web.json_response(
         {
